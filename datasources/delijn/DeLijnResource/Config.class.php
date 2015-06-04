450b8384214eb1186e03b24579852afa35bbe6c0
brechtvdv <br5cht@hotmail.com>
2015-06-04 11:25:41 +0200

    Change database config

--- datasources/delijn/DeLijnResource/Config.class.php
+++ datasources/delijn/DeLijnResource/Config.class.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Please configure this file by filling out the right elements to connect to the database containing the DeLijn data and copy this to Config.class.php file
- * @package The-DataTank
- * @copyright (C) 2011 by iRail vzw/asbl
- * @license AGPLv3
- * @author Maarten Cautreels <maarten@flatturtle.com>
+ * 
+ * @package DeLijnResource
+ * @copyright (C) 2015 by iRail vzw/asbl
+ * @author Brecht Van de Vyvere <brecht@iRail.be
+ * @license MIT
  */
 class DeLijnConfig {
-    public static $DB = 'mysql:host=192.168.70.71;dbname=irail-datadelijn';
+    public static $DB = 'mysql:host=192.168.70.70;dbname=irail-datadelijn';
     public static $DB_USER = 'root';
-    public static $DB_PASSWORD = '';
+    public static $DB_PASSWORD = 'root';
 }
 ?>
--- datasources/tec/TECResource/Config.class.php
+++ datasources/tec/TECResource/Config.class.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Please configure this file by filling out the right elements to connect to the database containing the DeLijn data and copy this to Config.class.php file
- *
+ * 
+ * @package TECResource
  * @copyright (C) 2015 by iRail vzw/asbl
- * @license AGPLv3
- * @author Brecht Van de Vyvere <brecht@iRail.be>
+ * @author Brecht Van de Vyvere <brecht@iRail.be
+ * @license MIT
  */
 class TECConfig {
-    public static $DB = 'mysql:host=192.168.70.71;dbname=irail-datatec';
+    public static $DB = 'mysql:host=192.168.70.70;dbname=irail-datatec';
     public static $DB_USER = 'root';
-    public static $DB_PASSWORD = '';
+    public static $DB_PASSWORD = 'root';
 }
 ?>
